418bccc511046418f2eaf4216328168e5cc31b8b
Justin Fitzsimmons <justinf@nulogy.com>
2017-02-11 16:40:40 -0500

    Separates some logic, moves associate() to the keepasshttp module

--- src/keepasshttp/mod.rs
+++ src/keepasshttp/mod.rs
@@ -5,6 +5,15 @@ use self::rand::{ Rng, OsRng };
 
 extern crate base64;
 
+extern crate serde_json;
+
+extern crate hyper;
+use self::hyper::Client;
+use self::hyper::header::{Headers, ContentType, Accept};
+
+use std::io::{self, Read, Write};
+use std::io::prelude::*;
+
 #[derive(Serialize, Deserialize, Debug)]
 struct TestAssociateRequest {
     #[serde(rename = "Nonce")]
@@ -30,7 +39,7 @@ struct TestAssociateResponse {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct AssociateRequest {
+struct AssociateRequest {
     #[serde(rename = "RequestType")]
     request_type: String,
 
@@ -84,4 +93,51 @@ struct RawEntry {
     password: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub key: String,
+    pub id: String,
+}
 
+pub fn associate() -> Result<Config, String> {
+    let associate_request = AssociateRequest::new();
+    let body = serde_json::to_string(&associate_request).unwrap();
+    // println!("{}", body); // TODO: figure out how to do debug output properly
+    let client = Client::new();
+    let mut res = client.post("http://localhost:19455").
+        header(ContentType::json()).
+        header(Accept::json()).
+        body(body.as_str()).
+        send().
+        unwrap();
+
+    let mut buf = String::new();
+    res.read_to_string(&mut buf).unwrap();
+
+    let associate_response: AssociateResponse = serde_json::from_str(buf.as_str()).unwrap();
+
+    match associate_response.success {
+        true => Ok(Config { key: associate_request.key.to_owned(), id: associate_response.id.unwrap().to_owned() }),
+        false => Err(String::from("Association request did not succeed. User canceled, or protocol error."))
+    }
+}
+
+// fn test_associate() -> String {
+//     let mut key: [u8; 32] = [0; 32];
+//     let mut nonce: [u8; 16] = [0; 16];
+//     let mut rng = OsRng::new().ok().unwrap();
+//     rng.fill_bytes(&mut key);
+//     rng.fill_bytes(&mut nonce);
+
+//     let verifier = keepasshttp::crypto::encrypt(&nonce, &key, &nonce).unwrap();
+
+//     let req = TestAssociateRequest {
+//         nonce: base64::encode(&nonce),
+//         verifier: base64::encode(&verifier),
+//         request_type: String::from("test-associate"),
+//         trigger_unlock: false,
+//         id: String::from("PHP"),
+//     };
+
+//     serde_json::to_string(&req).unwrap()
+// }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ extern crate rustc_serialize;
 extern crate docopt;
 use docopt::Docopt;
 
-extern crate hyper;
-use hyper::Client;
-use hyper::header::{Headers, ContentType, Accept};
-
 extern crate rand;
 use rand::{ Rng, OsRng };
 
@@ -43,12 +39,6 @@ struct Args {
     flag_username_only: bool
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Config {
-    key: String,
-    id: String,
-}
-
 fn config_path() -> PathBuf {
     env::var("CMDIPASS_CONFIG").map(|e| PathBuf::from(e)).unwrap_or_else(|_| {
         let mut pathbuf = env::home_dir().unwrap_or(PathBuf::from(""));
@@ -61,73 +51,20 @@ fn config_exists() -> bool {
     config_path().as_path().exists()
 }
 
-fn load_config() -> io::Result<Config> {
+fn load_config() -> io::Result<keepasshttp::Config> {
     let mut res = File::open(config_path())?;
     let mut buf = String::new();
     res.read_to_string(&mut buf)?;
 
-    let config: Config = serde_json::from_str(buf.as_str())
+    let config: keepasshttp::Config = serde_json::from_str(buf.as_str())
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, format!("Invalid JSON: {}", e.description())))?;
     Ok(config)
 }
 
-fn write_config_file(request: &keepasshttp::AssociateRequest, response: &keepasshttp::AssociateResponse) {
-    let config = Config {
-        key: request.key.to_owned(),
-        id: response.id.to_owned().unwrap(),
-    };
+fn write_config_file(config: &keepasshttp::Config) {
     let mut file = File::create(config_path()).unwrap();
     file.write_all(serde_json::to_string(&config).unwrap().as_bytes()).unwrap();
 }
-
-// fn test_associate() -> String {
-//     let mut key: [u8; 32] = [0; 32];
-//     let mut nonce: [u8; 16] = [0; 16];
-//     let mut rng = OsRng::new().ok().unwrap();
-//     rng.fill_bytes(&mut key);
-//     rng.fill_bytes(&mut nonce);
-
-//     let verifier = keepasshttp::crypto::encrypt(&nonce, &key, &nonce).unwrap();
-
-//     let req = TestAssociateRequest {
-//         nonce: base64::encode(&nonce),
-//         verifier: base64::encode(&verifier),
-//         request_type: String::from("test-associate"),
-//         trigger_unlock: false,
-//         id: String::from("PHP"),
-//     };
-
-//     serde_json::to_string(&req).unwrap()
-// }
-
-fn associate() {
-    writeln!(io::stderr(), "Config file not found at '{}', generating new key and registering with server", config_path().to_string_lossy()).unwrap();
-
-    let associate_request = keepasshttp::AssociateRequest::new();
-    let body = serde_json::to_string(&associate_request).unwrap();
-    println!("{}", body);
-    let client = Client::new();
-    let mut res = client.post("http://localhost:19455").
-        header(ContentType::json()).
-        header(Accept::json()).
-        body(body.as_str()).
-        send().
-        unwrap();
-
-    let mut buf = String::new();
-    res.read_to_string(&mut buf).unwrap();
-
-    let associate_response: keepasshttp::AssociateResponse = serde_json::from_str(buf.as_str()).unwrap();
-
-    match associate_response.success {
-        true => write_config_file(&associate_request, &associate_response),
-        false => {
-            writeln!(io::stderr(), "Association request did not succeed. User canceled, or protocol error").unwrap();
-            std::process::exit(1);
-        }
-    }
-}
-
 
 fn main() {
     let args: Args = Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit());
@@ -142,14 +79,14 @@ fn main() {
         process::exit(0);
     }
 
-    let client = Client::new();
-    let mut res = client.get("http://localhost").send().unwrap();
-    let mut buf = String::new();
-    res.read_to_string(&mut buf).unwrap();
-
     if !config_exists() {
-        associate();
+        writeln!(io::stderr(), "Config file not found at '{}', generating new key and registering with server", config_path().to_string_lossy()).unwrap();
+        match keepasshttp::associate() {
+            Ok(config) => write_config_file(&config),
+            Err(e) => {
+                writeln!(io::stderr(), "{}", e);
+                process::exit(1);
+            }
+        }
     }
-
-
 }
