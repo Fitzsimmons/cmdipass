19f06dbacc8220804f56d9f18a1828ea22e8a4ce
Justin Fitzsimmons <justin@fitzsimmons.ca>
2017-04-30 23:31:11 -0400

    Uses a better way to get the version in the package script

--- package.bash
+++ package.bash
@@ -6,15 +6,15 @@ if [[ "$OS" = "Windows_NT" ]]; then
 	extension=".exe"
 fi
 
-set -e
-set -x
+set -exo pipefail
 
-triple=$(rustup show | head -n1 | cut -d ' ' -f3)
-version=$(grep 'const VERSION' src/main.rs | cut -d '"' -f 2)
+triple=$(rustup show | head -n1 | cut -d ' ' -f3 || true) # crashes in windows for some reason, but still gives output
 
 mkdir -p package
 
 cargo build
+
+version=$(cargo run -- --version | sed s/cmdipass-//)
 
 keybase sign -d -i target/debug/cmdipass$extension -o target/debug/cmdipass$extension.sig.saltpack
 keybase pgp sign -d -i target/debug/cmdipass$extension -o target/debug/cmdipass$extension.sig.pgp
