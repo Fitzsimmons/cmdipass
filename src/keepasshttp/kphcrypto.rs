99da5c70d90e5956cf2f8476e8de03c550efd61d
Justin FItzsimmons <justin@fitzsimmons.ca>
2018-04-11 11:34:17 -0400

    Improve attribution of kphcrypto.rs

--- src/keepasshttp/kphcrypto.rs
+++ src/keepasshttp/kphcrypto.rs
@@ -1,10 +1,40 @@
+// Copied verbatim from the rust-crypto sample code, which coincidentally is exactly what we need
+// https://github.com/DaGenix/rust-crypto/blob/master/examples/symmetriccipher.rs
+// Actually we don't need the buffers to be 4k like they are in the example
+
+// Copyright (c) 2006-2009 Graydon Hoare
+// Copyright (c) 2009-2013 Mozilla Foundation
+
+// Permission is hereby granted, free of charge, to any
+// person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the
+// Software without restriction, including without
+// limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following
+// conditions:
+
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
 extern crate crypto as crypto_rs;
 
 use self::crypto_rs::{ symmetriccipher, buffer, aes, blockmodes };
 use self::crypto_rs::buffer::{ ReadBuffer, WriteBuffer, BufferResult };
-// Copied verbatim from the rust-crypto sample code, which coincidentally is exactly what we need
-// https://github.com/DaGenix/rust-crypto/blob/master/examples/symmetriccipher.rs
-// Actually we don't need the buffers to be 4k like they are in the example
+
+
 pub fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
     let mut encryptor = aes::cbc_encryptor(aes::KeySize::KeySize256, key, iv, blockmodes::PkcsPadding);
 
